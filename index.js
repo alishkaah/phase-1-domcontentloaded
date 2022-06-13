9e289f06de6afdbc1eea56f59b99469a98622eb7
Moringa <alinoorshukri8@gmail.com>
2022-06-13 19:29:57 +0300

    commit tree 16f82dadbe57fc5a7c00d92a2c5016c3c3221cee

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let docs = document.querySelector( 'p' );
+    docs.textContent = 'This is really cool!'
+    console.log(docs)
+
+    
+  });
+  
+  
